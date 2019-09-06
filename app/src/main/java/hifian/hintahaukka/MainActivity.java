309044c8b94267cc3ed4f08a97e2fdec4b09e4d6
MWargelin <matias.wargelin@gmail.com>
2019-09-06 19:35:02 +0300

    Käynnistä viivakoodin skannaus etusivun näkymän napista

--- app/src/main/java/hifian/hintahaukka/MainActivity.java
+++ app/src/main/java/hifian/hintahaukka/MainActivity.java
@@ -2,22 +2,25 @@ package hifian.hintahaukka;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Intent intent = new Intent(this, Scanner.class);
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button scanButton = findViewById(R.id.button_scan_barcode);
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BarcodeScannerActivity.class);
+                startActivity(intent);
             }
         });
     }
